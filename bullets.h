84d9b3547b295c353ebf7638604a354761380a09
Student <bware@cs.csubak.edu>
2015-05-28 10:03:36 -0700

    minor error fix with swap files

--- bullets.h
+++ bullets.h
@@ -4,7 +4,7 @@
 //Handles bullet physics/creation/
 
 extern void fire_weapon(Game *game);
-extern void bulletDraw(Bullet *b, Game *g);
+extern void bulletDraw(Bullet *b);
 extern void bulletAng(Bullet *b);
 extern void updateBulletPos(Game *game, Bullet *b);
 extern void bul_zomb_collision(Game *g, Bullet *b);
